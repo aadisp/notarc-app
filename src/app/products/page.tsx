9ae86a0a6c8a6cfcf3f39ea1a38ebcac2594731e
Aadi <spaadi1601@gmail.com>
2026-07-30 16:43:22 +0530

    TTle

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Notarc | Products",
+};
+
 "use client";
 
 import { useProducts } from "@/hooks/use-products";
@@ -5,11 +11,6 @@ import { useMemo, useState } from "react";
 import SiteLayout from "@/components/layout/site-layout";
 import ProductGrid from "@/components/products/product-grid";
 import ProductToolbar from "@/components/products/product-toolbar";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Notarc | Products",
-};
 
 export default function ProductsPage() {
   const {
