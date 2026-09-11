03a89b5a33c1b9b0976c8d345b81d6349f43c9d7
Aadi <spaadi1601@gmail.com>
2026-09-11 20:05:01 +0530

    Test

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -374,9 +374,10 @@ export default function Navbar() {
 
               <div className="flex items-center gap-2">
 
-                {/* <Link
+                <Link
                   href="/login"
                   className="
+                    hidden
                     rounded-full
                     px-4
                     py-2
@@ -412,7 +413,7 @@ export default function Navbar() {
                   "
                 >
                   Signup
-                </Link> */}
+                </Link>
 
               </div>
 
